acda3aa64f053df889803caa47c2a4bcbbb3bb6e
Yujin Cho <yujincho2002@gmail.com>
2024-04-28 12:36:05 -0700

    stuff

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -14,7 +14,7 @@ const ECommerce: React.FC = () => {
         <PieChart />
         <LineChart/>
       </div>
-
+{/* 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5 mt-8">
         <CardDataStats title="Total views" total="$3.456K" rate="0.43%" levelUp>
           <svg
@@ -100,7 +100,7 @@ const ECommerce: React.FC = () => {
             />
           </svg>
         </CardDataStats>
-      </div>
+      </div> */}
 
       <div className="mt-10">
         <h2 className="text-title-md2 font-semibold text-black dark:text-white mb-6 ml-1">
